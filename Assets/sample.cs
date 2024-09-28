41a8ae45c3c70ce033b7fb9be4018cd67e75fd87
yanagikaito <voltgt777@gmail.com>
2024-09-28 15:35:20 +0900

    1の値を表示

--- Assets/sample.cs
+++ Assets/sample.cs
@@ -7,12 +7,12 @@ public class sample : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int a = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
